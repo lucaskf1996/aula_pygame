f57139d9b1fa868836a49f795b24a32364a5e5b7
lucaskf1996 <lucaskf1996@gmail.com>
2019-04-27 20:54:58 -0300

    falta o pop up de pontos, coracoes e vida a cada tantos pontos

--- asteroids-base.py
+++ asteroids-base.py
@@ -6,15 +6,14 @@ import random
 from os import path
 import time
 
-# Estabelece a pasta que contem as figuras.
+# Estabelece a pasta que contem as figuras, sons e fontes
 img_dir = path.join(path.dirname(__file__), 'img')
-
-#Estabelece a pasta que contem as figuras
 snd_dir = path.join(path.dirname(__file__), "snd")
+fnt_dir = path.join(path.dirname(__file__), "font")
 
 # Dados gerais do jogo.
-WIDTH = 480 # Largura da tela
-HEIGHT = 600 # Altura da tela
+WIDTH = 700 # Largura da tela
+HEIGHT = 900 # Altura da tela
 FPS = 60 # Frames por segundo
 
 # Define algumas variáveis com as cores básicas
@@ -52,6 +51,7 @@ class Player(pygame.sprite.Sprite):
         
         #velocidade da nave
         self.speedx = 0
+        self.speedy = 0
         
         #Melhora a colisao estabelecendo um raio de um circulo
         self.radius = 25
@@ -60,12 +60,23 @@ class Player(pygame.sprite.Sprite):
     #metodo que atualiza a posicao da navinha
     def update(self):
         self.rect.x += self.speedx
+        self.rect.y += self.speedy
         
         #mantem dentro da tela
         if self.rect.right > WIDTH:
             self.rect.right = WIDTH
         if self.rect.left < 0:
             self.rect.left = 0
+        if self.rect.y > HEIGHT-50:
+            self.rect.y = HEIGHT-50
+        if self.rect.y < 30:
+            self.rect.y = 30
+
+
+
+
+
+
 
 class Mob(pygame.sprite.Sprite):
     
@@ -81,11 +92,18 @@ class Mob(pygame.sprite.Sprite):
         
         self.rect = self.image.get_rect()
         
+        velocidade_max_x = 5
+        velocidade_min_x = -5
+
         self.rect.y = random.randrange(-100,-40)
         self.rect.x = random.randrange(0, WIDTH)
         
-        self.speedx = random.randrange(-3,3)
-        self.speedy = random.randrange(2,9)
+        #quanto mais proximo de WIDTH mais eu retiro e quanto mais longe menos eu retiro
+        velocidade_max_x = velocidade_max_x - velocidade_max_x * self.rect.x/WIDTH
+        velocidade_min_x = velocidade_min_x - velocidade_min_x * (1 - self.rect.x/WIDTH)
+
+        self.speedx = random.uniform(velocidade_min_x, velocidade_max_x) #max 3 para x = 0 eh bom 
+        self.speedy = random.randrange(5,9)
         
         self.radius = int(self.rect.width * 0.7 / 2)
         
@@ -99,12 +117,17 @@ class Mob(pygame.sprite.Sprite):
             self.rect.y = random.randrange(-100,-40)
             self.rect.x = random.randrange(0, WIDTH)
                        
-            self.speedx = random.randrange(-3,3)
-            self.speedy = random.randrange(2,9)
+            velocidade_max_x = 5
+            velocidade_min_x = -5
+            velocidade_max_x = velocidade_max_x - velocidade_max_x * self.rect.x/WIDTH
+            velocidade_min_x = velocidade_min_x - velocidade_min_x * (1 - self.rect.x/WIDTH)
+
+            self.speedx = random.uniform(velocidade_min_x, velocidade_max_x)
+            self.speedy = random.randrange(5,9)
 
 class Laser(pygame.sprite.Sprite):
     
-    def __init__(self, laser_img):
+    def __init__(self, laser_img, pos_x, pos_y):
         
         pygame.sprite.Sprite.__init__(self)
         
@@ -113,6 +136,9 @@ class Laser(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         
         self.image.set_colorkey(BLACK)
+
+        self.rect.x = pos_x
+        self.rect.y = pos_y
         
         self.image = pygame.transform.scale(laser_img, (10, 30))
         
@@ -120,6 +146,8 @@ class Laser(pygame.sprite.Sprite):
         
         #Velocidade do laser
         self.rect.y -= 10
+        if self.rect.bottom < 0:
+            self.kill()
 
 class Explosion(pygame.sprite.Sprite):
     
@@ -190,6 +218,7 @@ def load_assets(img_dir, snd_dir):
         img.set_colorkey(BLACK)
         explosion_anim.append(img)
     assets["explosion_anim"] = explosion_anim
+    assets["score_font"] = pygame.font.Font(path.join(fnt_dir, "PressStart2P.ttf"), 28)
     return assets
     
 
@@ -208,7 +237,7 @@ assets = load_assets(img_dir, snd_dir)
 
 player = Player(assets["player_img"])
 mob = Mob(assets["mob_img"])
-laser = Laser(assets["laser_img"])
+laser = Laser(assets["laser_img"], player.rect.x, player.rect.y)
 
 
 #variavel para o ajuste de velocidade
@@ -216,6 +245,7 @@ clock = pygame.time.Clock()
 
 # Carrega o fundo do jogo
 background = assets["background"]
+background = pygame.transform.scale(background, (WIDTH, HEIGHT))
 background_rect = background.get_rect()
 
 #Carrega os sons do Jogo
@@ -227,12 +257,14 @@ laser_pew = assets["laser_pew"]
 
 player = Player(assets["player_img"]) 
 
+#carrega a fonte para desenhar o score
+score_font = assets["score_font"]
 #cria um grupo de sprites e adiciona a nave
 all_sprites = pygame.sprite.Group()
 all_sprites.add(player)
 
 mobs = pygame.sprite.Group()
-for i in range(0,8):
+for i in range(0,15):
     mob = Mob(assets["mob_img"])
     all_sprites.add(mob)
     mobs.add(mob)
@@ -242,6 +274,8 @@ lista_laser = pygame.sprite.Group()
 #Flags dos botoes
 right_key=0
 left_key=0
+down_key=0
+up_key=0
 
 
 # Comando para evitar travamentos.
@@ -249,9 +283,13 @@ try:
     #Inicia a musica
     pygame.mixer.music.play(loops=-1)
 
+    score = 0
+
     PLAYING = 0
     EXPLODING = 1
     DONE = 2
+
+    vidas = 0
 
     state = PLAYING
     # Loop principal.
@@ -277,15 +315,17 @@ try:
                     if event.key == pygame.K_RIGHT:
                         right_key = 1
                         player.speedx = 8
+                    if event.key == pygame.K_DOWN:
+                        down_key = 1
+                        player.speedy = 3
+                    if event.key == pygame.K_UP:
+                        up_key = 1
+                        player.speedy = -3
                     if event.key == pygame.K_SPACE:
                         #Atira
-                        laser = Laser(assets["laser_img"])
+                        laser = Laser(assets["laser_img"], player.rect.x + 20, player.rect.y)
                     
                         laser_pew.play()
-                    
-                        #colocando o laser no lugar do jogador
-                        laser.rect.x = player.rect.x + 20
-                        laser.rect.y = player.rect.y
                     
                         #adiciona laser pra lista de laser
                         all_sprites.add(laser)
@@ -300,13 +340,22 @@ try:
                         player.speedx = 0
                         if right_key == 1:
                             player.speedx = 8
-                    
                     if event.key == pygame.K_RIGHT:
                         right_key = 0
                         player.speedx = 0
                         if left_key == 1:
                             player.speedx = -8
-                        
+                    if event.key == pygame.K_UP:
+                        up_key = 0
+                        player.speedy = 0
+                        if down_key == 1:
+                            player.speedx = 3
+                    if event.key == pygame.K_DOWN:
+                        down_key = 0
+                        player.speedy = 0
+                        if up_key == 1:
+                            player.speedx = -3
+
         #depois de processar os eventos
         #atualiza a acao de cada sprite
         all_sprites.update()
@@ -324,7 +373,7 @@ try:
                 all_sprites.add(explosao)
                 state = EXPLODING
                 explosion_tick = pygame.time.get_ticks()
-                explosion_duration  = explosao.frame_ticks * len(explosao.explosion_anim) + 400
+                explosion_duration = explosao.frame_ticks * len(explosao.explosion_anim) + 400
             
             
             hits2 = pygame.sprite.groupcollide(lista_laser, mobs, True, True)
@@ -332,24 +381,38 @@ try:
             for hit in hits2:
             
                 mob = Mob(assets["mob_img" ])
-                laser_pew.play()
                 mobs.add(mob)
                 all_sprites.add(mob)
                 laser_hit.play()
  
                 explosao = Explosion(hit.rect.center, assets["explosion_anim"])
                 all_sprites.add(explosao)
+
+                score += 100
         
         elif state == EXPLODING:
             now = pygame.time.get_ticks()
             if now - explosion_tick > explosion_duration:
-                state = DONE 
+                vidas += 1
+                if vidas > 2:
+                    state = DONE
+                else:
+                    player = Player(assets["player_img"])
+                    all_sprites.add(player)
+                    state = PLAYING
+                
 
         # A cada loop, redesenha o fundo e os sprites
+        
         screen.fill(BLACK)
         screen.blit(background, background_rect)
         all_sprites.draw(screen)
         
+        #desenha o score
+        text_surface = score_font.render("{:08d}".format(score), True, YELLOW)
+        text_rect = text_surface.get_rect()
+        text_rect.midtop = (WIDTH / 2, 10)
+        screen.blit(text_surface, text_rect)
         # Depois de desenhar tudo, inverte o display.
         pygame.display.flip()
         
